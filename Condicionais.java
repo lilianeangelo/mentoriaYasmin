ad99129871647930fc9d3127b64fbf8c5e6e8a27
liliane.angelo <liliane.angelomartins@gmail.com>
2022-10-11 19:05:35 -0300

    inserindo atividades

---
+++ Condicionais.java
@@ -0,0 +1,18 @@
+
+
+/*1) Crie um programa no qual consiga calcular a média das 5 notas de 3 alunos e 
+retorne "Reprovado" caso seja menor que 6*/
+
+/*2) Crie um programa que simule uma lampada acesa, sendo A para acesa e D para desligada */
+
+/*3) Crie um programa que receba as informações dos lados de um triangulo e retorne se é isósceles, equilatero ou escaleno*/
+
+/*4) Crie um programa que calcule as raízes de uma equação de segundo grau, na forma ax²+bx+c, 
+recebendo os valores de a, b, c. Se a=0, não é uma equação de segundo grau, 
+e o programa não pode pedir mais valores. Se delta for negativo, não existem raízes 
+e o programa deve ser encerrado. Se o delta for igual a zero, só possui uma raiz real. 
+E se for positivo, possui duas raizes reais. Não esqueça de informar ao usuário */
+
+public class Condicionais {
+    
+}
---
+++ PrimeiroCalculo.java
@@ -0,0 +1,13 @@
+/*Exercício proposto*/
+
+/*1) Crie um programa no qual consiga calcular a média das 5 notas de 3 alunos*/
+
+/*2) Crie um programa no qual consiga calcular a área de um polígono*/
+
+/*3) Crie um programa que retorne o resultado da raiz quadrada de um número*/
+
+/*4) Crie um programa que recebe um caracter e me retorne se é uma vogal ou uma consoante */
+
+public class PrimeiroCalculo {
+    
+}
